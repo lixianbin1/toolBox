b34740462a8b0d8de8e1a8f9e0a06f87588986a1
xianbin <xianbin.me@qq.com>
2022-04-26 14:43:08 +0800

    add:添加详细备注

--- tools.js
+++ tools.js
@@ -1,4 +1,9 @@
-// 节流函数； throttle(fun,time)  fun:需要执行的函数 time:节流的时间间隔
+/*
+  name: prevent
+  remarks: throttle(fun,time)  fun:需要执行的函数 time:节流的时间间隔
+  author: lixianbin1
+  update: 2022/04/26
+*/
 var isThro=true
 export function throttle(fun,time){
   if(isThro){
@@ -10,7 +15,12 @@ export function throttle(fun,time){
   }
 }
 
-// 防抖函数： prevent(fun,time) fun:需要执行的函数 time:抖动的时间间隔
+/*
+  name: prevent
+  remarks: prevent(fun,time) fun:需要执行的函数 time:抖动的时间间隔
+  author: lixianbin1
+  update: 2022/04/26
+*/
 var isPrev=true,setTime
 var setTimeFun=(time)=>{
   clearTimeout(setTime)
@@ -28,12 +38,23 @@ export function prevent(fun,time){
   }
 }
 
-// 转换函数；将数字转换为 以3位数字并以逗号为间隔的字符串
+
+/*
+  name: toMoney
+  remarks: 处理数据格式，将数字转换为 以3位数字并以逗号为间隔的字符串，符合人民币风格
+  author: lixianbin1
+  update: 2022/04/26
+*/
 export function toMoney(num){
   return parseFloat(num).toLocaleString();
 }
 
-// 处理时间格式，起始和结束
+/*
+  name: timestampToTime
+  remarks: 处理时间格式，根据后台规则，查询数据的时间需要时分秒，给予起始时间和结束时间，
+  author: lixianbin1
+  update: 2022/04/26
+*/
 export function timestampToTime(timestamp, type=1) {
   try{
     var date = new Date(timestamp);
@@ -55,7 +76,33 @@ export function timestampToTime(timestamp, type=1) {
 
 
 //时间相关
-export function getTimes(type=":"){ //获取时分秒
+
+/*
+  name: newDate
+  remarks: 格式当前数据，生成符合中国的时间显示格式
+  author: lixianbin1
+  update: 2022/04/26
+*/
+export function newDate(time){
+  var a= new Date();
+  if(time){a=new Date(time)}
+  var y=a.getFullYear();
+  var m=a.getMonth()+1;
+  var d=a.getDate();
+  var h=a.getHours();
+  var f=a.getMinutes();
+  h<10?h="0"+h:h=h;
+  f<10?f="0"+f:f=f;
+  return y+"-"+m+"-"+d+" "+h+":"+f
+}
+
+/*
+  name: getTimes
+  remarks: 获取时分秒，时间字符串
+  author: lixianbin1
+  update: 2022/04/26
+*/
+export function getTimes(type=":"){
   var a= new Date();
   var h=a.getHours();
   var f=a.getMinutes();
@@ -65,7 +112,14 @@ export function getTimes(type=":"){ //获取时分秒
   s<10?s="0"+s:s=s;
   return h+type+f+type+s+""
 }
-export function getTime(type=":"){ //获取时分
+
+/*
+  name: getTime
+  remarks: 获取时分，时间字符串
+  author: lixianbin1
+  update: 2022/04/26
+*/
+export function getTime(type=":"){
   var a= new Date();
   var h=a.getHours();
   var f=a.getMinutes();
@@ -73,7 +127,14 @@ export function getTime(type=":"){ //获取时分
   f<10?f="0"+f:f=f;
   return h+type+f+""
 }
-export function getDay(type="-"){ //获取年月日
+
+/*
+  name: getDay
+  remarks: 获取年月日，时间字符串
+  author: lixianbin1
+  update: 2022/04/26
+*/
+export function getDay(type="-"){
   var a= new Date();
   var y=a.getFullYear();
   var m=a.getMonth()+1;
@@ -85,7 +146,14 @@ export function getDay(type="-"){ //获取年月日
   }
   return y+type+m+type+d+""
 }
-export function getDate(){ //获取年月日时分秒
+
+/*
+  name: getDate
+  remarks: 获取年月日时分秒，时间字符串
+  author: lixianbin1
+  update: 2022/04/26
+*/
+export function getDate(){
   var a= new Date();
   var y=a.getFullYear();
   var m=a.getMonth()+1;
@@ -100,6 +168,13 @@ export function getDate(){ //获取年月日时分秒
   s<10?s="0"+s:s=s;
   return y+"-"+m+"-"+d+" "+h+":"+f+":"+s
 }
+
+/*
+  name: manyYear
+  remarks: 获取两个时间相隔多少年
+  author: lixianbin1
+  update: 2022/04/26
+*/
 export function manyYear(time1,time2){ //获取两个时间相隔多少年
   try{
     if (time1 && time2) {
@@ -120,7 +195,14 @@ export function manyYear(time1,time2){ //获取两个时间相隔多少年
     console.error('Illegal parameter!')
   }
 }
-export function manyMonth(time1,time2){ //获取两个时间相隔多少月
+
+/*
+  name: manyMonth
+  remarks: 获取两个时间相隔多少月
+  author: lixianbin1
+  update: 2022/04/26
+*/
+export function manyMonth(time1,time2){
   try{
     if (time1 && time2) {
       let c =
@@ -144,7 +226,14 @@ export function manyMonth(time1,time2){ //获取两个时间相隔多少月
     console.error('Illegal parameter!')
   }
 }
-export function manyDay(time1,time2){ //获取两个时间相隔多少天
+
+/*
+  name: manyDay
+  remarks: 获取两个时间相隔多少天
+  author: lixianbin1
+  update: 2022/04/26
+*/
+export function manyDay(time1,time2){
   try{
     if (time1 && time2) {
       let c =
@@ -156,7 +245,13 @@ export function manyDay(time1,time2){ //获取两个时间相隔多少天
   }
 }
 
-export function leapyear(year){ //计算是否是闰年
+/*
+  name: leapyear
+  remarks: 计算是否是闰年
+  author: lixianbin1
+  update: 2022/04/26
+*/
+export function leapyear(year){
   if(new String(year).length==4){
     let a=year%100
     if(a>0){
@@ -179,27 +274,24 @@ export function leapyear(year){ //计算是否是闰年
   }
 }
 
-export function newDate(time){
-  var a= new Date();
-  if(time){a=new Date(time)}
-  var y=a.getFullYear();
-  var m=a.getMonth()+1;
-  var d=a.getDate();
-  var h=a.getHours();
-  var f=a.getMinutes();
-  h<10?h="0"+h:h=h;
-  f<10?f="0"+f:f=f;
-  return y+"-"+m+"-"+d+" "+h+":"+f
-}
-
-// 关闭函数：关闭当前窗口
+/*
+  name: closeWin
+  remarks: 关闭当前浏览器的窗口
+  author: lixianbin1
+  update: 2022/04/26
+*/
 export function closeWin(){
   window.opener=null;
   window.open('','_self');
   window.close();
 }
 
-// 深拷贝函数：deepClone(source) source:被拷贝的复杂对象
+/*
+  name: deepClone
+  remarks: 深拷贝，拷贝对象，且分配不同的内存地址；deepClone(source) source:被拷贝的复杂对象
+  author: lixianbin1
+  update: 2022/04/26
+*/
 export function deepClone(source){
   var targetObj = source.constructor === Array ? [] : {};
   for(let keys in source){
@@ -215,7 +307,12 @@ export function deepClone(source){
   return targetObj;
 }
 
-//反编译函数：防止用户通过断点调试
+/*
+  name: doLoop
+  remarks: 防止用户通过浏览器打开控制台进行断点调试
+  author: lixianbin1
+  update: 2022/04/26
+*/
 export function doLoop(){
   let a="((function() { var callbacks = [],timeLimit = 50,open = false;setInterval(loop, 1);return {addListener: function(fn) {callbacks.push(fn);},cancleListenr: function(fn) {callbacks = callbacks.filter(function(v) {return v !== fn;});}}function loop() {var startTime = new Date();`+`d`+`e`+`b`+`u`+`g`+`g`+`e`+`r`+`;if (new Date() - startTime > timeLimit) {if (!open) {callbacks.forEach(function(fn) {fn.call(null);});}open = true;window.stop();window.location.reload();} else {open = false;}}})()).addListener(function() {window.location.reload();});"
   eval(a)
